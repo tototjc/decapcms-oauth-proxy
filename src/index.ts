07c1bae02c5cdbd06c08cdac30fe286ac738474c
tototjc <53078240+tototjc@users.noreply.github.com>
2024-10-14 11:49:13 +0800

    feat: update scopes in auth url based on request param

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export default {
       }
       const state = generateState()
       const authUrl = await github.createAuthorizationURL(state, {
-        scopes: ['user', 'repo'],
+        scopes: searchParams.get('scopes')?.split(' '),
       })
       return Response.redirect(authUrl.toString(), 302)
     }
