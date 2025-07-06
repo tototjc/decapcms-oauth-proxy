a9149172c88b0e539fcd657cf452dc25353b1f00
tototjc <53078240+tototjc@users.noreply.github.com>
2025-07-06 18:19:19 +0800

    feat: add GitLab support to authentication middleware and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { env } from 'hono/adapter'
 import { createMiddleware } from 'hono/factory'
 import { HTTPException } from 'hono/http-exception'
 import { setCookie, getCookie, deleteCookie } from 'hono/cookie'
-import { GitHub, ArcticFetchError, OAuth2RequestError } from 'arctic'
+import { GitHub, GitLab, ArcticFetchError, OAuth2RequestError } from 'arctic'
 
 import { generateToken, verifyToken } from './csrf-token'
 
@@ -12,19 +12,37 @@ const DEFAULT_SITE_ID_LIST = ['localhost', '127.0.0.1']
 interface AppEnv extends HonoEnv {
   Bindings: Env
   Variables: {
-    github: GitHub
+    provider: 'github' | 'gitlab'
+    authClient: GitHub | GitLab
   }
 }
 
-const githubAuthMiddleware = createMiddleware<AppEnv>(async (ctx, next) => {
-  ctx.set(
-    'github',
-    new GitHub(
-      env(ctx).GITHUB_OAUTH_ID,
-      env(ctx).GITHUB_OAUTH_SECRET,
-      new URL('/callback', ctx.req.url).href
+const authMiddleware = createMiddleware<AppEnv>(async (ctx, next) => {
+  const provider = ctx.req.query('provider')
+  if (provider == 'github') {
+    ctx.set('provider', provider)
+    ctx.set(
+      'authClient',
+      new GitHub(
+        env(ctx).GITHUB_OAUTH_ID,
+        env(ctx).GITHUB_OAUTH_SECRET,
+        new URL(`/callback?provider=${provider}`, ctx.req.url).href
+      )
     )
-  )
+  } else if (provider == 'gitlab') {
+    ctx.set('provider', provider)
+    ctx.set(
+      'authClient',
+      new GitLab(
+        'https://gitlab.com',
+        env(ctx).GITLAB_OAUTH_ID,
+        env(ctx).GITLAB_OAUTH_SECRET,
+        new URL(`/callback?provider=${provider}`, ctx.req.url).href
+      )
+    )
+  } else {
+    throw new HTTPException(400, { message: 'Invalid provider' })
+  }
   await next()
   const err = ctx.error
   if (err instanceof OAuth2RequestError) {
@@ -34,6 +52,13 @@ const githubAuthMiddleware = createMiddleware<AppEnv>(async (ctx, next) => {
     throw new HTTPException(500, { message: 'Network error', cause: err })
   }
 })
+
+const renderBody = (provider: string, status: 'success' | 'error', content: Record<string, unknown>) => `
+<script>
+  window.addEventListener('message', () => window.opener.postMessage('authorization:${provider}:${status}:${JSON.stringify(content)}', '*'), { once: true })
+  window.opener.postMessage('authorizing:${provider}', '*')
+</script>
+`.trim()
 
 const app = new Hono<AppEnv>()
 
@@ -45,60 +70,63 @@ app.onError((err, ctx) => {
   }
 })
 
-app.get('/auth', githubAuthMiddleware, async ctx => {
-  const allowSiteIdList = [
-    ...env(ctx).ALLOW_SITE_ID_LIST.trim().split(','),
-    ...DEFAULT_SITE_ID_LIST,
-  ]
-  const { site_id, provider, scope } = ctx.req.query()
-  const refererHost = URL.parse(ctx.req.header('Referer') ?? '')?.hostname
-  if (!refererHost || !allowSiteIdList.includes(refererHost)) {
-    throw new HTTPException(400, { message: 'Invalid referer' })
+app.get(
+  '/auth',
+  async (ctx, next) => {
+    const allowSiteIdList = [
+      ...env(ctx).ALLOW_SITE_ID_LIST.trim().split(','),
+      ...DEFAULT_SITE_ID_LIST,
+    ]
+    const site_id = ctx.req.query('site_id')
+    const refererHost = URL.parse(ctx.req.header('Referer') ?? '')?.hostname
+    if (!refererHost || !allowSiteIdList.includes(refererHost)) {
+      throw new HTTPException(400, { message: 'Invalid referer' })
+    }
+    if (!site_id || !allowSiteIdList.includes(site_id)) {
+      throw new HTTPException(400, { message: 'Invalid site_id' })
+    }
+    await next()
+  },
+  authMiddleware,
+  async ctx => {
+    const scope = ctx.req.query('scope')?.split(' ') ?? []
+    const state = await generateToken(env(ctx).SECRET)
+    const cookieName = `${ctx.var.provider}-state`
+    setCookie(ctx, cookieName, state, {
+      prefix: 'secure',
+      secure: true,
+      sameSite: 'Lax',
+      path: '/callback',
+      maxAge: 3 * 60,
+    })
+    return ctx.redirect(ctx.var.authClient.createAuthorizationURL(state, scope))
   }
-  if (!site_id || !allowSiteIdList.includes(site_id)) {
-    throw new HTTPException(400, { message: 'Invalid site_id' })
-  }
-  if (provider !== 'github') {
-    throw new HTTPException(400, { message: 'Invalid provider' })
-  }
-  const state = await generateToken(env(ctx).SECRET)
-  setCookie(ctx, 'auth-state', state, {
-    prefix: 'secure',
-    secure: true,
-    sameSite: 'Lax',
-    path: '/callback',
-    maxAge: 3 * 60,
-  })
-  return ctx.redirect(
-    ctx.var.github.createAuthorizationURL(state, scope ? scope.split(' ') : [])
-  )
-})
+)
 
-app.get('/callback', githubAuthMiddleware, async ctx => {
-  const { state, code } = ctx.req.query()
-  if (!code) {
-    throw new HTTPException(400, { message: 'Invalid code' })
+app.get(
+  '/callback',
+  authMiddleware,
+  async ctx => {
+    const cookieName = `${ctx.var.provider}-state`
+    const storedState = getCookie(ctx, cookieName, 'secure')
+    deleteCookie(ctx, cookieName, { secure: true, path: '/callback' })
+    const state = ctx.req.query('state')
+    if (
+      !state ||
+      !storedState ||
+      state !== storedState ||
+      !(await verifyToken(env(ctx).SECRET, state))
+    ) {
+      throw new HTTPException(400, { message: 'Invalid state' })
+    }
+    const code = ctx.req.query('code')
+    if (!code) {
+      throw new HTTPException(400, { message: 'Invalid code' })
+    }
+    const tokens = await ctx.var.authClient.validateAuthorizationCode(code)
+    return ctx.html(renderBody(ctx.var.provider, 'success', { token: tokens.accessToken() }))
   }
-  const storedState = getCookie(ctx, 'auth-state', 'secure')
-  if (
-    !state ||
-    !storedState ||
-    state !== storedState ||
-    !(await verifyToken(env(ctx).SECRET, state))
-  ) {
-    throw new HTTPException(400, { message: 'Invalid state' })
-  }
-  const tokens = await ctx.var.github.validateAuthorizationCode(code)
-  deleteCookie(ctx, 'auth-state', { secure: true, path: '/callback' })
-  return ctx.html(`
-    <script>
-      window.addEventListener('message', () => window.opener.postMessage('authorization:github:success:${JSON.stringify(
-        { token: tokens.accessToken() }
-      )}', '*'), { once: true })
-      window.opener.postMessage('authorizing:github', '*')
-    </script>
-  `)
-})
+)
 
 app.all('*', ctx => ctx.body('Ciallo～(∠·ω< )⌒★', 418))
 
